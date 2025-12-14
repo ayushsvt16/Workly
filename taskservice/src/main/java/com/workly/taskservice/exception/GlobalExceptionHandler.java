50c3114b621f78bfd883decdb751ffae1459ce3b
ayushsvt16 <ayush.230103011@iiitbh.ac.in>
2025-12-14 17:33:04 +0530

    Initialised swagger UI http://localhost:8081/swagger-ui/index.html

---
+++ taskservice/src/main/java/com/workly/taskservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.workly.taskservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .toList();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Validation failed");
+        response.put("errors", errors);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ taskservice/src/test/java/com/workly/taskservice/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workly.taskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.workly.taskservice.TaskServiceApplication;
+
+@SpringBootTest(classes = TaskServiceApplication.class)
+class TaskServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
